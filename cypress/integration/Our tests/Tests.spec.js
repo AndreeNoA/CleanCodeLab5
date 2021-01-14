c85af7f731196a555671e960174f158208b84a4b
AndreeNoA <55133543+AndreeNoA@users.noreply.github.com>
2021-01-14 15:56:17 +0100

    Added cypress and test cases

---
+++ cypress/integration/Our tests/Tests.spec.js
@@ -0,0 +1,32 @@
+﻿describe('Clean Code Lab 5 Tests', () => {
+    it('Find id of original bug in db', () => {
+        cy.visit('http://localhost:1234');
+        cy.get('#tableBugs').eq(0).should('contain', '8722df98-f574-4d3a-b7b4-38d7c062cd5a')
+    })
+
+    it('Create new bug', () => {
+        cy.get('#createBugLink').click()
+        cy.get('#bugTextInput').type("Testbug")
+        cy.get('#authorInput').type("TestbugAuthor")
+        cy.get('#submitBug').click();
+    })
+
+    it('Update bug', () => {
+        cy.contains('Testbug').parent('tr').within(() => {
+            cy.get('td').eq(3).contains('Edit').click()
+        })
+        cy.get('#editBugText').type("Updated text")
+        cy.get('#editBugSubmit').click()
+    })
+
+    it('Delete bug', () => {
+        cy.contains('Updated text').parent('tr').within(() => {
+            cy.get('td').eq(4).contains('Delete').click()
+        })
+    })
+
+    it('Count', () => {
+        //cy.get('.parentClass').get('.childClass').should('have.length', expectedCount);
+        cy.get('#bodyOfBugs').find('tr').should('have.length', 1)
+    })
+})
